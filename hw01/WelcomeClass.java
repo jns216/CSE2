a72d154fcde42e330daeabaf95036703327703a9
jns216 <jns216@lehigh.edu>
2015-01-26 01:06:22 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//////////////////////////////////////
+// Jillian Saydah
+// hw 01
+// Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+//  define a class
+public class WelcomeClass{
+    
+// add main method
+public static void main(String[] args) {
+    
+// print the welcome screen below
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-J--N--S--2--1--6->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("I am from a small town 45 minutes outside of Boston. I live with my parents and a younger sister who attends the University of Massachusetts Amherst");
+}
+}
